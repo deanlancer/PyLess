5113e315c54d1015c45e13be005905f6604b8c48
Deanlancer <deanlancer@gmail.com>
2024-10-01 09:57:04 +0300

    main: format with Black

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print('Hello, World!')
-print(5//5)
+print("Hello, World!")
+print(5 // 5)
 print(type(5))
-print(2.3+2.3)
-print(int('5'))
-print(type(int('5')))
-
+print(2.3 + 2.3)
+print(int("5"))
+print(type(int("5")))
