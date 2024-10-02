4db62e73ea35006eab5dd40e0eebddc8ae49d263
Deanlancer <deanlancer@gmail.com>
2024-10-02 18:24:01 +0300

    Test 10.02
    *Практическое задание по теме "Переменные"

---
+++ variables.py
@@ -0,0 +1,14 @@
+finished_hmwrks = 12
+spent_hrs = 1.5
+course_name = "Python"
+time_spnt_pr = 1.5 / 12
+print(
+    "Курс:",
+    course_name,
+    "всего задач:",
+    finished_hmwrks,
+    "затрачено часов:",
+    spent_hrs,
+    "среднее время выполнения:",
+    time_spnt_pr,
+)
