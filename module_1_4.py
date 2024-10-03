b19991f42f284e3eec1797586fd840e5fa740302
Deanlancer <deanlancer@gmail.com>
2024-10-03 19:32:44 +0300

    Practice for 10.03
    *"Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+my_string = input("What is orange and sounds like a parrot? ")
+print("Right answer is orange carrot. Your answer is", my_string)
+print("Right answer is carrot. Your answer is", my_string.upper())
+print("Right answer is carrot. Your answer is", my_string.lower())
+print("Right answer is carrot. Your answer is", my_string.replace(' ', ''))
+print("Right answer is carrot. Your answer is", my_string[0])
+print("Right answer is carrot. Your answer is", my_string[-1])
+
+
+
+
