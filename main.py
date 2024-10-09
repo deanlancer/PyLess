a2ba03b349704e107b3605101d20083fc99360a7
Deanlancer <deanlancer@gmail.com>
2024-10-09 08:43:41 +0300

    Practice 10.09
    *Цели и задачи. Поток выполнения программы

--- main.py
+++ main.py
@@ -22,3 +22,9 @@ print(float("123.456"))
 print(float("123.456") * 10)
 print(int(float("123.456") * 10))
 print(int(float("123.456") * 10) % 10)
+
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
